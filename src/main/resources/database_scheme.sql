706798a484a240be3efae1b218534a04aee95824
Michael Terletskyi <michaelterletskyiofficial@gmail.com>
2021-06-27 13:29:51 +0300

    SQL database_scheme was updated

--- src/main/resources/database_scheme.sql
+++ src/main/resources/database_scheme.sql
@@ -18,25 +18,19 @@ CREATE TABLE EMPLOYEES
     email           VARCHAR(255) NOT NULL UNIQUE,
     salary_per_hour DECIMAL(7, 1),
     date_of_birth   DATE,
-    department_id   BIGINT       NULL,
-    CONSTRAINT departmentEmployeesIndex
-        FOREIGN KEY (department_id) REFERENCES DEPARTMENTS (department_id)
+    head            TINYINT(1),
+    department_id   BIGINT       NULL REFERENCES DEPARTMENTS (department_id)
 );
-
-CREATE INDEX departmentEmployeesIndex
-    ON EMPLOYEES (department_id);
-
-
 
 INSERT INTO DEPARTMENTS(title)
 VALUES ('Groove Street Department'),
        ('Los Santos Police Department');
 
-INSERT INTO EMPLOYEES(first_name, last_name, email, salary_per_hour, date_of_birth, department_id)
-VALUES ('Carl', 'Johnson', 'cj@gmail.com', 250000, '1968-05-03', 1),
-       ('Sean', 'Johnson', 'sweet@gmail.com', 30, '1966-03-11', 1),
-       ('Melvin', 'Harris', 'bigsmoke@yahoo.com', 100, '1967-10-29', 1),
-       ('Lance', 'Wilson', 'ryder@yahoo.com', 50, '1963-06-24', 1),
+INSERT INTO EMPLOYEES(first_name, last_name, email, salary_per_hour, date_of_birth, head, department_id)
+VALUES ('Carl', 'Johnson', 'cj@gmail.com', 250000, '1968-05-03', 1, 1),
+       ('Sean', 'Johnson', 'sweet@gmail.com', 30, '1966-03-11', 0, 1),
+       ('Melvin', 'Harris', 'bigsmoke@yahoo.com', 100, '1967-10-29', 0, 1),
+       ('Lance', 'Wilson', 'ryder@yahoo.com', 50, '1963-06-24', 0, 1),
 
-       ('Frank', 'Tenpenny', 'officertenpenny@gmail.com', 80.0, '1958-11-12', 2),
-       ('Edward', 'Pulaski', 'officerpulaski@gmail.com', 80.0, '1956-02-27', 2)
+       ('Frank', 'Tenpenny', 'officertenpenny@gmail.com', 80.0, '1958-11-12', 1, 2),
+       ('Edward', 'Pulaski', 'officerpulaski@gmail.com', 80.0, '1956-02-27', 0, 2)
